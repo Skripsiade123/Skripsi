163b46ec175941e5d76ab161c55bb143e2801a02
Skripsiade123 <skripsiade5@gmail.com>
2025-06-20 10:04:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,12 +31,10 @@ hide_streamlit_style = """
     h3:contains("Pra-pemrosesan Data:") {
         display: none !important;
     }
-    .stAlert[data-testid="stSidebar"] { /* Ini menargetkan spesifik alert di sidebar, tapi .stAlert lebih umum */
-        display: none !important;
-    }
+  
     </style>
     """
-
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 @st.cache_data
 def load_data():
