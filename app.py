32498482433d2ec3386f41de4bce459e34324053
Skripsiade123 <skripsiade5@gmail.com>
2025-06-20 10:26:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ hide_streamlit_style = """
 
     </style>
     """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=False)
 
 # ... (sisa kode aplikasi Streamlit Anda) ...
 
